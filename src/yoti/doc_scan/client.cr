dd11fd59e72a632337845e6c547a030039f52b99
Jeremy Woertink <jeremywoertink@gmail.com>
2024-08-20 14:57:22 -0700

    Refactor message signature to use the correct auth signature

--- src/yoti/doc_scan/client.cr
+++ src/yoti/doc_scan/client.cr
@@ -10,8 +10,7 @@ module Yoti
       end
 
       def create_session(payload : Yoti::DocScan::SessionSpecification) : Session
-        endpoint = url_for("sessions")
-        Yoti::SignedRequest.post(endpoint, payload) do |response|
+        Yoti::SignedRequest.post("sessions", payload) do |response|
           Session.from_json(response.body)
         end
       end
@@ -19,24 +18,15 @@ module Yoti
       # NOTE: I'm returning JSON::Any here because mapping this output would be
       # super gnarly https://developers.yoti.com/identity-verification/results#result-of-the-session
       def get_session(session_id : UUID) : JSON::Any
-        endpoint = url_for("sessions/#{session_id}")
-        Yoti::SignedRequest.get(endpoint) do |response|
+        Yoti::SignedRequest.get("sessions/#{session_id}") do |response|
           JSON.parse(response.body)
         end
       end
 
       def get_media_content(session_id : UUID, media_id : String) : Yoti::Media
-        endpoint = url_for("sessions/#{session_id}/media/#{media_id}/content")
+        endpoint = "sessions/#{session_id}/media/#{media_id}/content"
         Yoti::SignedRequest.get(endpoint) do |response|
           Yoti::Media.new(response.body, response.headers["Content-Type"])
-        end
-      end
-
-      private def url_for(path : String) : String
-        String.build do |io|
-          io << Yoti.doc_scan_api_endpoint
-          io << "/#{path}"
-          io << "?sdkId=#{Yoti.settings.client_sdk_id}"
         end
       end
     end
--- src/yoti/signed_request.cr
+++ src/yoti/signed_request.cr
@@ -6,20 +6,30 @@ module Yoti
       File.read(Yoti.settings.key_file_path)
     end
 
-    def initialize(@method : String, @url : String, @payload : Yoti::DocScan::SessionSpecification)
+    @path : String
+
+    def initialize(@method : String, endpoint : String, @payload : Yoti::DocScan::SessionSpecification? = nil)
+      @path = "/#{endpoint}?sdkId=#{Yoti.settings.client_sdk_id}&nonce=#{UUID.random}&timestamp=#{Time.utc.to_unix}"
     end
 
-    def self.post(url : String, payload : Yoti::DocScan::SessionSpecification, &block : HTTP::Client::Response -> _)
-      self.new("POST", url, payload).exec(&block)
+    def self.post(endpoint : String, payload : Yoti::DocScan::SessionSpecification, &block : HTTP::Client::Response -> _)
+      self.new("POST", endpoint, payload).exec(&block)
     end
 
-    def self.get(url : String, &block : HTTP::Client::Response -> _)
-      self.new("GET", url).exec(&block)
+    def self.get(endpoint : String, &block : HTTP::Client::Response -> _)
+      self.new("GET", endpoint).exec(&block)
     end
 
     def exec(&)
-      HTTP::Client.exec(@method, @url, headers) do |response|
+      HTTP::Client.exec(@method, url_for(@path), headers) do |response|
         yield response
+      end
+    end
+
+    private def url_for(path : String) : String
+      String.build do |io|
+        io << Yoti.doc_scan_api_endpoint
+        io << path
       end
     end
 
@@ -32,14 +42,19 @@ module Yoti
       }
     end
 
+    # https://developers.yoti.com/identity-verification-api#authentication
     private def message_signature : String
       ssl = Yoti::SSL.new(self.class.private_key)
-      message = [@method, @url, base64_payload].join('&')
+      message = if data = @payload
+                  [@method, @path, base64_payload(data)].join('&')
+                else
+                  [@method, @path].join('&')
+                end
       ssl.sign(message)
     end
 
-    private def base64_payload : String
-      Base64.encode(@payload.to_json)
+    private def base64_payload(data) : String
+      Base64.encode(data.to_json)
     end
   end
 end
